d195e29835e446af8a87e71dc410671bf05de6ad
AKASH SRIVASTAVA <akash.s1684@gmail.com>
2016-05-23 10:55:18 +0530

    removing class

--- hash_map.py
+++ hash_map.py
@@ -1,41 +1,37 @@
-class HashTable(object):
-    def __init__(self):
-        self.table = [None]*10000
+table = [None]*10000
 
-    def store(self, string):
+def store( string):
         """Input a string that's stored in
         the table."""
-        hash_index = self.calculate_hash_value(string)
-        if self.table[hash_index]:
-            self.table[hash_index].append(string)
+        hash_index = calculate_hash_value(string)
+        if table[hash_index]:
+            table[hash_index].append(string)
         else:
-            self.table[hash_index]= [string]
+            table[hash_index]= [string]
 
-
-    def lookup(self, string):
+def lookup(string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
-        index = self.calculate_hash_value(string)
-        if self.table[index]:
-            for each_key in self.table[index]:
+        index = calculate_hash_value(string)
+        if table[index]:
+            for each_key in table[index]:
                 if each_key == string:
                     return string
         return -1
 
-    def calculate_hash_value(self, string):
+def calculate_hash_value(string):
         """Helper function to calulate a
         hash value from a string."""
         return ord(string[0])*100+ord(string[1])
 
 
 # Setup
-hash_table = HashTable()
 
-print (hash_table.calculate_hash_value('Akash'))
+print (calculate_hash_value('Akash'))
 
-print (hash_table.lookup('Akash'))
+print (lookup('Akash'))
 
 # Test store
-hash_table.store('Akash')
-print (hash_table.lookup('Akash'))
+store('Akash')
+print (lookup('Akash'))
