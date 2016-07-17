292d3a65150f9c61c6a1a59fe5f9109da70947c7
AKASH SRIVASTAVA <akash.s1684@gmail.com>
2016-07-17 23:34:54 +0530

    Converting all the file's extension in a given directory

--- ext_exchange.py
+++ ext_exchange.py
@@ -1,18 +1,19 @@
 import os
 
 path = input("PATH:")
-from_extension = '.jpg'
-to_extension = '.bmp'
+from_extension = '.bmp'
+to_extension = '.jpg'
 count=0
 
 print (path)
 listing = os.listdir(path)
 for filename in listing:
-    if not ( from_extension in filename):
+    if not ( from_extension in filename or from_extension.upper() in filename):
         pass
     else:
         #print ("current file is: " + filename)
         newname=filename.replace(from_extension,to_extension)
+        newname = filename.replace(from_extension.upper(), to_extension)
         #print (newname)
         os.rename(path+'\\'+filename,path+'\\'+newname)
         count=count+1;
