dadbd5db6da6edc04d066e90b5fcce3d98bcb92f
AKASH SRIVASTAVA <akash.s1684@gmail.com>
2016-08-01 13:34:07 +0530

    Update whatsapp_spam.py

--- whatsapp_spam.py
+++ whatsapp_spam.py
@@ -1,12 +1,13 @@
-from splinter import Browser
+#NOTE: for this script to work you have to first sign-in on Whatsapp web using QR code
 
+from splinter import Browser
 
 browser = Browser()
 browser.visit('https://web.whatsapp.com/')
-input('press enter to continue')
+input('press enter to continue')   #to make sure page is completely loaded 
 count=20;
 
-friend_list=["friend 1","friend 2","friend 3"]
+friend_list=["friend 1","friend 2","friend 3"]  #Whatsapp names of friends
 
 for friend in friend_list:
     xp='//span[contains(text(),'+friend+')]'
@@ -14,5 +15,5 @@ for friend in friend_list:
     chat.click()
     elem1 = browser.find_elements_by_class_name('input')
     for i in range(1,count):
-        elem1[1].send_keys('Whatsapp has expired')
+        elem1[1].send_keys('Whatsapp has expired')   #text to send
         browser.find_element_by_class_name('send-container').click()
